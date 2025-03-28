2945d27ee1fd2441997a755104b56072ff078a8b
VishvamPatel31 <96796901+VishvamPatel31@users.noreply.github.com>
2025-03-27 23:39:09 -0400

    added needed images and data

---
+++ data/data.js
@@ -0,0 +1,22 @@
+const menu = [
+    {
+        image: "images/pizza.png",
+        food: "Pizza",
+        toppings: ["pepperoni, mushroom, mozarella"],
+        price: 14
+    },
+
+    {
+        image: "images/burger.png",
+        food: "Hamburger",
+        toppings: ["beef, cheese, lettuce"],
+        price: 12
+    },
+
+    {
+        image: "beer/pizza.png",
+        food: "Beer",
+        toppings: ["grain, yeast, water"],
+        price: 14
+    }
+]
